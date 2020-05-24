340642209825144e08075930c12fba224139945c
J-H-Freedman <jacob@jhfreedman.com>
2020-05-24 19:30:44 -0400

    Structure HTML

--- 2020/constructor_header.php
+++
@@ -1,7 +0,0 @@
-</head>
-<header>
-    <a href="index.php"><img src="images/logo_black.svg" class="logo" alt="JF splash logo"></a>
-    <?php include("constructor_nav.html"); ?>
-</header>
-
-<body>

87f48c4fc3846a7f610576cdee46a0c8ccab7734
J-H-Freedman <j.h.freedman@icloud.com>
2024-03-05 02:54:59 -0500

    edit gitignore

--- _site/gallery/htmlToMd.ps1
+++
@@ -1,6 +0,0 @@
-$folderPath = 'E:\1_Documents\1_professional\website\gallery\AiGeneratedArt' # your actual folder path
-
-Get-ChildItem -Path $folderPath -Filter *.html | ForEach-Object {
-    $newName = $_.BaseName + ".md"
-    Rename-Item -Path $_.FullName -NewName $newName
-}
